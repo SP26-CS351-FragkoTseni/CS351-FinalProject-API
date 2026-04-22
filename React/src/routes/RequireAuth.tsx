6a8b3d8b013888f4fe6bd6429180f4275c0b4e96
Fragko Tseni <your-email@example.com>
2026-04-22 10:33:21 -0500

    delete React

--- React/src/routes/RequireAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { isAuthenticated } from '../core/auth/auth';
-
-export function RequireAuth() {
-  if (!isAuthenticated()) {
-    return <Navigate to="/login" replace />;
-  }
-  return <Outlet />;
-}
